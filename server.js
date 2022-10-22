f4b25c67e70ef83fc99e35416ee913bf96bd59ae
e145278 <tony.kim@wnco.com>
2022-10-22 17:39:29 -0500

    test

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const connectDB = require('./config/db');
 const errorHandler = require('./middlewares/error'); 
 const cookieParser = require('cookie-parser');
 const fileupload = require('express-fileupload');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet'); 
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 
 dotenv.config({ path: './config/config.env' }); 
 
@@ -30,6 +36,23 @@ app.use(cookieParser());
 //file upload middleware
 app.use(fileupload());
 
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(hpp());
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 mins
+    max: 100
+}); 
+
+app.use(limiter); 
+
+app.use(helmet());
+
+app.use(cors());
+
 app.use(logger);
 app.use('/api/v1/item', item);
 app.use('/api/v1/category', category);
@@ -39,11 +62,11 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5001; 
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is listening on PORT: ${PORT}`)
 }); 
 
 process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`); 
-    process.exit(1)
+    server.close(() => process.exit(1));
 })
